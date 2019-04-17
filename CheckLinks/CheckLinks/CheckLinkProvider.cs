15351d0a1a708f1ed4e28318f2282eb195be6dce
hoanvu9495 <hoanvu9495@gmail.com>
2019-04-17 20:36:17 +0700

    init

---
+++ CheckLinks/CheckLinks/CheckLinkProvider.cs
@@ -0,0 +1,44 @@
+﻿using CheckLinks.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CheckLinks
+{
+    public class CheckLinkProvider
+    {
+        public static async Task<SiteManager> Check(SiteManager obj)
+        {
+            if (obj!=null)
+            {
+                var rgx = new Regex(@"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$");
+               
+                if (obj.Link != null && rgx.IsMatch(obj.Link))
+                {
+                    var request = (HttpWebRequest)WebRequest.Create(obj.Link);
+                    request.Method = "HEAD";
+                    var response = (HttpWebResponse)request.GetResponse();
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        obj.Status = true;
+                    }
+                    else
+                    {
+                        obj.Status = false;
+                    }
+                }
+                else
+                {
+                    obj.Status = false;
+                }
+               
+            }
+           
+            return obj;
+        }
+    }
+}
---
+++ CheckLinks/CheckLinks/Program.cs
@@ -0,0 +1,25 @@
+﻿using CheckLinks.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckLinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db= new DBEntities();
+            var listSite = db.SiteManagers.ToList();
+            var lstTask= new List<Task<SiteManager>>();
+            foreach (var item in listSite)
+            {
+                lstTask.Add(CheckLinkProvider.Check(item));
+            }
+            Task.WhenAll(lstTask);
+            db.SaveChanges();
+        }
+    }
+}
